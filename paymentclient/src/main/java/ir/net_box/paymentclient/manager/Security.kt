cf08e951718b3604df735d13f02d601dd01fea41
Mrey3000 <34549616+Mrey3000@users.noreply.github.com>
2024-06-10 15:59:07 +0330

    add store public version signature

--- paymentclient/src/main/java/ir/net_box/paymentclient/manager/Security.kt
+++ paymentclient/src/main/java/ir/net_box/paymentclient/manager/Security.kt
@@ -2,44 +2,46 @@ package ir.net_box.paymentclient.manager
 
 import android.content.Context
 import android.content.pm.PackageManager
-import android.content.pm.Signature
 import android.os.Build
+import android.util.Log
 import ir.net_box.paymentclient.BuildConfig
 import ir.net_box.paymentclient.manager.AppManager.NET_STORE_PACKAGE_NAME
 import java.io.ByteArrayInputStream
-import java.io.InputStream
-import java.security.PublicKey
 import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
 import java.util.Locale
 
 object Security {
     fun verifyNetstoreIsInstalled(context: Context): Boolean {
-        val packageManager: PackageManager = context.packageManager
         val packageName = NET_STORE_PACKAGE_NAME
 
-        val signatures: Array<Signature> = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val packageInfo = packageManager.getPackageInfo(
+        val signatures = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val packageInfo = context.packageManager.getPackageInfo(
                 packageName,
                 PackageManager.GET_SIGNING_CERTIFICATES
             )
             packageInfo.signingInfo.apkContentsSigners
         } else {
-            val packageInfo = packageManager.getPackageInfo(
+            val packageInfo = context.packageManager.getPackageInfo(
                 packageName,
                 PackageManager.GET_SIGNATURES
             )
             packageInfo.signatures
         }
 
-        for (sig in signatures) {
-            val input: InputStream = ByteArrayInputStream(sig.toByteArray())
-            val certificateFactory: CertificateFactory = CertificateFactory.getInstance("X509")
-            val certificate: X509Certificate =
+        for (signature in signatures) {
+            val input = ByteArrayInputStream(signature.toByteArray())
+            val certificateFactory = CertificateFactory.getInstance("X509")
+            val certificate =
                 certificateFactory.generateCertificate(input) as X509Certificate
-            val publicKey: PublicKey = certificate.publicKey
-            val certificateHex = byte2HexFormatted(publicKey.encoded)
-            if (BuildConfig.NETBOX_CERTIFICATE != certificateHex) {
+            val certificateHex = byte2HexFormatted(certificate.publicKey.encoded)
+
+            val validCertificates = listOf(
+                BuildConfig.NETBOX_CERTIFICATE,
+                BuildConfig.NETBOX_PUBLIC_CERTIFICATE
+            )
+
+            if (certificateHex !in validCertificates) {
                 return false
             }
         }
